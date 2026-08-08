0ca0c190a969e123c2572a98e1aaebb129283403
DakoOlexiniozfetg365343 <sashadakopro@gmail.com>
2026-08-08 15:04:59 +0300

    changed requestDto to make streaming and added func in interface

--- MyWebApi/DTO/OpenRouterRequestDto.cs
+++ MyWebApi/DTO/OpenRouterRequestDto.cs
@@ -2,4 +2,6 @@ public class OpenRouterRequestDto
 {
     public string Model {get;set;} = string.Empty;
     public List<OpenRouterMessageDto>? Messages {get;set;}
+
+    public bool Stream {get;set;} = true;
 }
--- MyWebApi/Services/Abstractions/IOpenRouterService.cs
+++ MyWebApi/Services/Abstractions/IOpenRouterService.cs
@@ -1,4 +1,5 @@
 public interface IOpenRouterService
 {
     public Task<string> GetCompletionAsync (string userPrompt,string systemInstruction,string model);
+    public IAsyncEnumerable<string> GetCompletionStreamAsync (string userPrompt, string systemInstruction, string model);
 }
